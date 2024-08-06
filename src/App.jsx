7606ba36e44fdaab15336c39a7a6c9b3596ecf80
615shoaib <615shoaib@gmail.com>
2024-08-06 10:29:39 -0700

    latest commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Login from './Pages/Login'
 import Homepage from './Pages/Homepage'
 import { Route, Routes } from 'react-router-dom'
 import Campian from './Pages/Campian'
+import BasicSetting from './Pages/BasicSetting'
 
 
 const App = () => {
@@ -17,6 +18,7 @@ const App = () => {
       <Route path='/' element={<Homepage />}/>
       <Route path='/login' element={<Login />} />
       <Route path='/campion' element={<Campian />} />
+      <Route path='/Basic/Setting' element={<BasicSetting />} />
     </Routes>
 
     <Footer />
---
+++ src/Pages/BasicSetting.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BasicSetting = () => {
+  return (
+    <>
+   <h1>Helo world</h1>
+    </>
+  )
+}
+
+export default BasicSetting
